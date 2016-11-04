bb01b19dd8774d5aca21a26ceadba3f2ade60425
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-11-04 01:27:58 -0700

    finished checkpoint

--- scripts/album.js
+++ scripts/album.js
@@ -63,12 +63,52 @@
      }
  };
 
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+var clickHandler = function(targetElement) {
+    var songItem = getSongItem(targetElement);
+    
+    if (currentlyPlayingSong === null) {
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+    } else if  ( currentlyPlayingSong === songItem.getAttribute('data-song-number')){
+        songItem.innerHTML = playButtonTemplate;
+        currentlyPlayingSong = null;
+    } else if ( currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number')
+        
+    }
+};
+
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
-//album button 
+//album Play button 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+//Pause Button 
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+//store state of playing songs
+var currentlyPlayingSong = null; 
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
@@ -76,12 +116,43 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
      songListContainer.addEventListener('mouseover', function(event) {
          if (event.target.parentElement.className === 'album-view-song-item'){
              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+             var songItem = getSongItem(event.target);
+             if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                 songItem.innerHTML = playButtonTemplate;
+             }
          }
      });
      
      for (var i = 0; i < songRows.length; i++){
          songRows[i].addEventListener('mouseleave', function(event) {
-            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number'); 
+            var songItem = getSongItem(event.target);
+            var songItemNumber = songItem.getAttribute('data-song-number');
+             
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+         });
+         
+         songRows[i].addEventListener('click', function(event) {
+           clickHandler(event.target);  
          });
      }
  };
+
+
+
+
+
+var findParentByClassName = function(element, targetClass) {
+    if (element.parentElement) {
+        var currentParent = element.parentElement;
+        while (currentParent && currentParent.className != targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        if (currentParent && (currentParent.className === targetClass)) return currentParent;
+        alert("No parent found with that class name");
+    } else {
+        alert("No Parent Element")
+    }
+};
+
